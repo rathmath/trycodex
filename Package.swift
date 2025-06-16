ddee588074d3fe8d37d92a0db976dc83747e44bc
rathmath <scholar.lambs34@icloud.com>
2025-06-15 18:38:30 -0700

    restructure package to standard layout

--- Package.swift
+++ Package.swift
@@ -3,10 +3,11 @@ import PackageDescription
 
 let package = Package(
     name: "swift_library",
-    dependencies: [
-        .package(path: "MyLibrary")  // Reference to the local package.
+    products: [
+        .library(name: "MyLibrary", targets: ["MyLibrary"]),
     ],
     targets: [
+        .target(name: "MyLibrary"),
         .executableTarget(
             name: "swift_library",
             dependencies: ["MyLibrary"])
